b9dbe53714bfd356ca0d71ad12826a741ba46f08
hp <bindureddy4755@gmail.com>
2021-04-28 15:31:00 +0530

    get student by id

--- springbootrest/src/main/java/com/example/demo/StudentServices.java
+++ springbootrest/src/main/java/com/example/demo/StudentServices.java
@@ -3,24 +3,28 @@ package com.example.demo;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Service
 public class StudentServices {
-	
-	@RequestMapping("students")
-	public List<Student> getStudents()
-	{
-		List<Student> students = new ArrayList<>();
-		students.add(new Student(1,"himabindu",8,89));
-		students.add(new Student(2,"bindu",8,86));
-		students.add(new Student(3,"bindu",8,84));
-		students.add(new Student(4,"himani",8,89));
-		
-		
+	List<Student> students = new ArrayList<>();
+	public void createStudents() {
+		students.add(new Student(1, "himabindu", 1, 11));
+		students.add(new Student(2, "bindu", 2, 22));
+		students.add(new Student(3, "hima",3, 33));
+		students.add(new Student(4, "himani", 4, 44));
+	}
+	public List<Student> getStudents(){
+		createStudents();
 		return students;
-		
+	}
+
+	public Student getStudent(int id) {
+		// TODO Auto-generated method stub
+		Student student=  students.stream().filter(s -> s.getId() == id ).findFirst().get();
+		return student;
 	}
 
 }
